305cec9177497564e6b1a4ef34c7e1fe6588c9d3
Nisha Krishna <nishakrishna2019@yahoo.com>
2019-10-06 20:51:48 -0500

    Add files via upload

--- comprehensions.py
+++ comprehensions.py
@@ -1,39 +1,12 @@
-fish = "halibut"
+names = []
+for _ in range(5):
+    name = input("Please enter the name of someone you know. ")
+    names.append(name)
 
-# Loop through each letter in the string
-# and push to an array
-letters = []
-for letter in fish:
-    letters.append(letter)
+lowercased = [name.lower() for name in names]
+titlecased = [name.title() for name in lowercased]
+invitations = [
+    f"Dear {name}, please come to the wedding this Saturday!" for name in titlecased]
 
-print(letters)
-
-# List comprehensions provide concise syntax for creating lists
-letters = [letter for letter in fish]
-
-print(letters)
-
-# We can manipulate each element as we go
-capital_letters = []
-for letter in fish:
-    capital_letters.append(letter.upper())
-
-print(capital_letters)
-
-# List Comprehension for the above
-capital_letters = [letter.upper() for letter in fish]
-
-print(capital_letters)
-
-# We can also add conditional logic (if statements) to a list comprehension
-july_temperatures = [87, 85, 92, 79, 106]
-hot_days = []
-for temperature in july_temperatures:
-    if temperature > 90:
-        hot_days.append(temperature)
-print(hot_days)
-
-# List Comprehension with conditional
-hot_days = [temperature for temperature in july_temperatures if temperature > 90]
-
-print(hot_days)
+for invitation in invitations:
+    print(invitation)
